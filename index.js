c2246484ec3ff0aff93a8622db2e73bd79989a56
amberdoo11 <57081110+amberdoo11@users.noreply.github.com>
2019-12-04 13:30:23 -0700

    we completed assignment... mostly

--- index.js
+++ index.js
@@ -3,3 +3,15 @@
 $('.thumbnail').on('click', e => {
   console.log(e.target);
 });
+
+function handleClick() {
+  $('.thumbnail').click(function(event) {
+    var selectedImg = $(event.currentTarget).find('img').attr('src');
+    $('.hero img').attr('src', selectedImg);
+  });
+}
+
+$(function() {
+  handleClick();
+});
+
